0d54f8b140bb40eb96e4e7bd8a0c55e584addf3f
zachschroeder <zach.schroeder17@gmail.com>
2025-05-19 08:14:20 -0400

    Updated github action step verisons. Updated hello world response

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "message": "hello world from SAM"
         }),
     }
