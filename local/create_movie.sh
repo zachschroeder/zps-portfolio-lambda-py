0562812b730f537b76a70a95211dec9cabf82b17
Zach Schroeder <zach.schroeder17@gmail.com>
2025-08-03 16:04:40 -0400

    Updated create_movie.sh

--- local/create_movie.sh
+++ local/create_movie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-requestData="{\"movie_id\":\"$(uuidgen)\",\"title\":\"Tanner sucks\",\"director\":\"Mr. Director\"}"
+requestData="{\"movie_id\":\"$(uuidgen)\",\"title\":\"The Truman Show\",\"director\":\"Mr. Director\"}"
 echo $requestData
 
 curl --header "Content-Type: application/json" \
